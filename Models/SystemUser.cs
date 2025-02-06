88237ec18fd39fa40f008687808c1ddcf52b709e
DrChrisHax <cmanlove1234@outlook.com>
2025-02-06 12:39:50 -0800

    model_base created

---
+++ Models/SystemUser.cs
@@ -0,0 +1,12 @@
+﻿namespace HandwritingNeuralNetwork.Models
+{
+    public class SystemUser : model_base
+    {
+        public int ID_SystemUser { get; set; }
+        public string UserName { get; set; }
+
+
+        public SystemUser() { }
+        public SystemUser(int id) : base(id) { }
+    }
+}
