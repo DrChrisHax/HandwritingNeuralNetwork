571a9f3b1dac0508804393a661662824a7e23986
DrChrisHax <cmanlove1234@outlook.com>
2025-02-25 10:56:37 -0800

    Stubbed out grid

---
+++ AIModel/DrawingGrid.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace HandwritingNeuralNetwork.AIModel
+{
+    public partial class DrawingGrid : UserControl
+    {
+        private const int gridRows = 64;
+        private const int gridColumns = 64;
+        private const int cellSize = 10;
+        private bool[,] cells;
+
+        public DrawingGrid()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- AIModel/ctlAIInput.Designer.cs
+++ AIModel/ctlAIInput.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.lblTitle = new System.Windows.Forms.Label();
+            this.btnAnalyze = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lblTitle
@@ -36,20 +37,38 @@
             this.lblTitle.AutoSize = true;
             this.lblTitle.Font = new System.Drawing.Font("Arial Rounded MT Bold", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblTitle.Location = new System.Drawing.Point(15, 15);
-            this.lblTitle.Margin = new System.Windows.Forms.Padding(3);
+            this.lblTitle.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.lblTitle.Name = "lblTitle";
             this.lblTitle.Size = new System.Drawing.Size(330, 27);
             this.lblTitle.TabIndex = 0;
             this.lblTitle.Text = "Handwriting Neural Network";
             // 
+            // btnAnalyze
+            // 
+            this.btnAnalyze.AutoSize = true;
+            this.btnAnalyze.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnAnalyze.BackColor = System.Drawing.Color.LimeGreen;
+            this.btnAnalyze.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnAnalyze.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAnalyze.Location = new System.Drawing.Point(15, 60);
+            this.btnAnalyze.Name = "btnAnalyze";
+            this.btnAnalyze.Size = new System.Drawing.Size(203, 31);
+            this.btnAnalyze.TabIndex = 1;
+            this.btnAnalyze.Text = "Analyze Handwriting";
+            this.btnAnalyze.UseMnemonic = false;
+            this.btnAnalyze.UseVisualStyleBackColor = false;
+            // 
             // ctlAIInput
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 27F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.Controls.Add(this.btnAnalyze);
             this.Controls.Add(this.lblTitle);
+            this.Font = new System.Drawing.Font("Arial Rounded MT Bold", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.Name = "ctlAIInput";
-            this.Size = new System.Drawing.Size(852, 574);
+            this.Size = new System.Drawing.Size(934, 571);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -58,5 +77,6 @@
         #endregion
 
         private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Button btnAnalyze;
     }
 }
