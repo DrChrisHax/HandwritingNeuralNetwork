73514ea5827c24515c17473f56c27aed485c1c39
DrChrisHax <cmanlove1234@outlook.com>
2025-03-10 18:48:02 -0700

    Stubbed out AI Training Suite Page

---
+++ AIModel/TrainingSuite/IViewTrainingSuite.cs
@@ -0,0 +1,13 @@
+﻿using HandwritingNeuralNetwork.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandwritingNeuralNetwork.AIModel.TrainingSuite
+{
+    public interface IViewTrainingSuite : IViewControlBase
+    {
+    }
+}
---
+++ AIModel/TrainingSuite/ctlTrainingSuite.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HandwritingNeuralNetwork.AIModel.TrainingSuite
+{
+    public partial class ctlTrainingSuite : UserControl, IViewTrainingSuite
+    {
+        public ctlTrainingSuite()
+        {
+            InitializeComponent();
+        }
+
+        public UserControl GetControlSurface()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetController(object controller)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
