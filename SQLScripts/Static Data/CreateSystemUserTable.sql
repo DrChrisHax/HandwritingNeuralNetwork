9391d45ed093e52bc12b6e220a707aa1c8b9d16b
Steve Truong <paogat123@csu.fullerton.edu>
2025-03-11 22:47:36 -0700

    Added CreatedSystemUserTable.sql and PopulateUsers.sql

---
+++ SQLScripts/Static Data/CreateSystemUserTable.sql
@@ -0,0 +1,58 @@
+﻿
+IF NOT EXISTS (
+    SELECT 1
+    FROM INFORMATION_SCHEMA.TABLES
+    WHERE TABLE_NAME = 'SystemUser'
+      AND TABLE_SCHEMA = 'dbo'
+)
+BEGIN
+    CREATE TABLE dbo.SystemUser
+    (
+        ID_SystemUser INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+        UserName      NVARCHAR(50) NOT NULL
+    );
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT 1
+    FROM INFORMATION_SCHEMA.COLUMNS
+    WHERE TABLE_NAME = 'SystemUser'
+      AND COLUMN_NAME = 'Password'
+      AND TABLE_SCHEMA = 'dbo'
+)
+BEGIN
+    ALTER TABLE dbo.SystemUser
+    ADD Password NVARCHAR(255);
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT 1
+    FROM INFORMATION_SCHEMA.COLUMNS
+    WHERE TABLE_NAME = 'SystemUser'
+      AND COLUMN_NAME = 'DateCreated'
+      AND TABLE_SCHEMA = 'dbo'
+)
+BEGIN
+    ALTER TABLE dbo.SystemUser
+    ADD DateCreated DATETIME;
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT 1
+    FROM INFORMATION_SCHEMA.COLUMNS
+    WHERE TABLE_NAME = 'SystemUser'
+      AND COLUMN_NAME = 'IsAdmin'
+      AND TABLE_SCHEMA = 'dbo'
+)
+BEGIN
+    ALTER TABLE dbo.SystemUser
+    ADD IsAdmin BIT;
+END;
+GO
+
+-- Test that the table and columns exist
+SELECT * FROM dbo.SystemUser;
+GO
---
+++ SQLScripts/Static Data/PopulateUsers.sql
@@ -0,0 +1,18 @@
+﻿IF NOT EXISTS (SELECT 1 FROM dbo.SystemUser WHERE UserName = 'admin')
+BEGIN
+    INSERT INTO dbo.SystemUser
+    (
+        UserName,
+        [Password],
+        DateCreated,
+        IsAdmin
+    )
+    VALUES
+    (
+        'admin',
+        'Admin@123',    
+        GETDATE(),
+        1
+    );
+END;
+GO
