718c2b9601ae13fd1382e92d454db221877406e4
DrChrisHax <cmanlove1234@outlook.com>
2025-03-18 10:18:26 -0700

    Updated login button

--- Login/LoginController.cs
+++ Login/LoginController.cs
@@ -3,11 +3,8 @@ using HandwritingNeuralNetwork.App;
 using HandwritingNeuralNetwork.Models;
 using HandwritingNeuralNetwork.Shared;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace HandwritingNeuralNetwork.Login
 {
