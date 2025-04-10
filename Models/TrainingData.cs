b05e41a34299a589af86566e4c6ba8898ed5a048
Steve Truong <paogat123@csu.fullerton.edu>
2025-04-09 19:02:18 -0700

    Match Variables from SQL and changed bool data to raw binary

--- Models/TrainingData.cs
+++ Models/TrainingData.cs
@@ -1,55 +1,74 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data.SqlClient;
+using System.Text;
 
 namespace HandwritingNeuralNetwork.Models
 {
     public class TrainingData : model_base
     {
+        // Public member properties
         public int ID_TrainingData { get; set; }
-        public bool[] Data { get; set; }
-        public int Score { get; set; }
+        public string Input { get; set; }
+        public string DataLabel { get; set; }
+        public DateTime LastUpdated { get; set; }
 
+        // Parameterless constructor
         public TrainingData() { }
+
+        // Constructor to load a record based on the primary key
         public TrainingData(int id) : base(id) { }
 
-        public string BoolArrayToString()
-        {
-            if (Data == null)
-                return string.Empty;
-            return string.Join("", Array.ConvertAll(Data, b => b ? "1" : "0"));
-        }
-
-
+       
+        /// Loads all records from the TrainingData table.
+        /// <returns>A list of TrainingData objects.</returns>
         public static List<TrainingData> LoadAll()
         {
-            List<TrainingData> dataList = new List<TrainingData>();
-            string tableName = nameof(TrainingData);
-            string sql = $"SELECT * FROM {tableName}";
-
-            string connectionString = $"Server=localhost\\HWNN;Database=HWNN;User Id=sa;Password=abc123!@#;";
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            {
-                connection.Open();
-                using (SqlCommand command = new SqlCommand(sql, connection))
-                {
-                    using (SqlDataReader reader = command.ExecuteReader())
-                    {
-                        while (reader.Read())
-                        {
-                            TrainingData td = new TrainingData();
-
-                            td.LoadPropertiesFromReader(reader);
-                            dataList.Add(td);
-                        }
-                    }
-                }
-            }
-            return dataList;
+            // Implementation will be added in the appropriate location.
+            throw new NotImplementedException();
         }
 
+        
+        /// Converts a bool array to a raw binary string.
+      
+        public static string BoolArrayToString(bool[] boolArray)
+        {
+            if (boolArray == null || boolArray.Length == 0)
+                return string.Empty;
 
+            StringBuilder sb = new StringBuilder();
+            int index = 0;
+
+            // Process complete groups of 8 bits
+            while (index + 8 <= boolArray.Length)
+            {
+                byte b = 0;
+                for (int bit = 0; bit < 8; bit++)
+                {
+                    if (boolArray[index + bit])
+                    {
+                        b |= (byte)(1 << (7 - bit));
+                    }
+                }
+                sb.Append((char)b);
+                index += 8;
+            }
+
+            // Process any remaining bits (pad the remaining bits with zeroes)
+            if (index < boolArray.Length)
+            {
+                byte b = 0;
+                int remaining = boolArray.Length - index;
+                for (int bit = 0; bit < remaining; bit++)
+                {
+                    if (boolArray[index + bit])
+                    {
+                        b |= (byte)(1 << (7 - bit));
+                    }
+                }
+                sb.Append((char)b);
+            }
+
+            return sb.ToString();
+        }
     }
 }
-
-
