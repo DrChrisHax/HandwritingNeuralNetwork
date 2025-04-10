50fb54c166bf66c51e0a17a75fec60e6ea3c15c5
NewUsername <ezraagale@csu.fullerton.edu>
2025-04-10 10:43:48 -0700

    save cells/classification -> update UI to display digit

--- AIModel/TrainingSuite/trainingSuiteController.cs
+++ AIModel/TrainingSuite/trainingSuiteController.cs
@@ -21,6 +21,31 @@ namespace HandwritingNeuralNetwork.AIModel.TrainingSuite
         {
             //Here we need to save the cells and classification in the db
             //Then update the UI to display the count of each classification digit
+            
+            // Step 1: Save the grid of cells and the classification label to the database
+            var cellData = ConvertCellsToDatabaseFormat(cells);
+
+            // Save the data to the database
+            SaveToDatabase(cellData, classification);
+
+            // Step 2: Update the classification counts in the database
+            var classificationCounts = GetClassificationCountsFromDatabase();
+
+            // Increment the count for the given classification
+            if (classificationCounts.ContainsKey(classification))
+            {
+                classificationCounts[classification]++;
+            }
+            else
+            {
+                classificationCounts[classification] = 1;
+            }
+
+            // Update the counts in the database
+            UpdateClassificationCountsInDatabase(classificationCounts);
+
+            // Step 3: Update the UI to display the new counts for each classification digit
+            View.UpdateClassificationCounts(classificationCounts);
         }
 
 
